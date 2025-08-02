6edfe42ed02688b737dd39a74d8e37c6680b96e0
TegarXLu <botdimas67@gmail.com>
2025-08-02 21:45:43 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # Kernel name
-KERNEL_NAME="QuartiX"
+KERNEL_NAME="TegarXLu"
 # Kernel Build variables
 USER="eraselk"
 HOST="gacorprjkt"
 TIMEZONE="Asia/Makassar"
 # AnyKernel
-ANYKERNEL_REPO="https://github.com/bintang774/anykernel"
-ANYKERNEL_BRANCH="gki"
+ANYKERNEL_REPO="https://github.com/TegarXLu/AnyKernel3"
+ANYKERNEL_BRANCH="main"
 # Kernel Source
-KERNEL_REPO="https://github.com/bintang774/gki-android12-5.10"
-KERNEL_BRANCH="quartix/main"
+KERNEL_REPO="https://github.com/TegarXLu/android_kernel_common-5.10"
+KERNEL_BRANCH="android12-5.10"
 KERNEL_DEFCONFIG="gki_defconfig"
 # Release repository
-GKI_RELEASES_REPO="https://github.com/bintang774/quartix-releases"
+GKI_RELEASES_REPO="https://github.com/TegarXLu/X669C-Release"
 # Clang
 CLANG_URL="$(./clang.sh mandi-sa)"
 CLANG_BRANCH=""
