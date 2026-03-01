47843ad148d8324d6a334618c7bef7e95f4dc1fc
TegarXLu <botdimas67@gmail.com>
2026-03-01 20:27:45 +0700

    Modify kernel defconfig selection in build.sh
    Update kernel defconfig logic for version 6.6.

--- build.sh
+++ build.sh
@@ -17,12 +17,12 @@ TIMEZONE="Asia/Jakarta"
 ANYKERNEL_REPO="https://github.com/TegarXLu/AnyKernel3"
 
 # Fixed Logic: 5.10 & 6.1 use gki_defconfig, others use quartix_defconfig
-if [ "$KVER" == "5.10" ]; then
-  KERNEL_DEFCONFIG="gki_defconfig"
+if [ "$KVER" == "6.6" ]; then
+  KERNEL_DEFCONFIG="rodin_defconfig"
 elif [ "$KVER" == "6.1" ]; then
   KERNEL_DEFCONFIG="gki_defconfig"
 else
-  KERNEL_DEFCONFIG="gki_defconfig"
+  KERNEL_DEFCONFIG="rodin_defconfig"
 fi
 
 if [ "$KVER" == "6.6" ]; then
