9738b5fad1128e4ddaa3a7bd565877115f7ac737
TegarXLu <botdimas67@gmail.com>
2026-03-01 20:28:17 +0700

    Update KSU inject script URL in build.sh

--- build.sh
+++ build.sh
@@ -79,7 +79,7 @@ DEFCONFIG_FILE=$(find ./arch/arm64/configs -name "$KERNEL_DEFCONFIG")
 log "Injecting custom KSU & SuSFS configs from GitHub..."
 export KSU
 export KSU_SUSFS
-wget -qO inject.sh https://raw.githubusercontent.com/Kingfinik98/gki-builder/refs/heads/6.x/inject_ksu/gki_defconfig.sh
+wget -qO inject.sh https://raw.githubusercontent.com/TegarXLu/gki-builder/refs/heads/6.x/inject_ksu/gki_defconfig.sh
 bash inject.sh
 rm inject.sh
 # --------------------------------------
