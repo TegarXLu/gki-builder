158f7873e58fe301be8069055e0db83473ac19bd
TegarXLu <botdimas67@gmail.com>
2026-03-01 21:19:31 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -214,7 +214,7 @@ if susfs_included; then
     SUSFS_DIR="$WORKDIR/susfs"
     SUSFS_PATCHES="${SUSFS_DIR}/kernel_patches"
     if [ "$KVER" == "6.6" ]; then
-      SUSFS_BRANCH=gki-android15-6.6-dev
+      SUSFS_BRANCH=gki-android15-6.6
     elif [ "$KVER" == "6.1" ]; then
       SUSFS_BRANCH=gki-android14-6.1
     elif [ "$KVER" == "5.10" ]; then
