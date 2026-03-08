cccebd22050e0698d797526b2659a80c5bf036bf
TegarXLu <botdimas67@gmail.com>
2026-03-08 19:11:11 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -179,7 +179,7 @@ elif [ "$KSU" == "resukisu" ]; then
   
   # Run the ReSukiSU setup script (using branch main)
   log "Running ReSukiSU setup from main branch..."
-  curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/Crowdin/kernel/setup.sh" | bash -s Crowdin
+  curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/Crowdin/kernel/setup.sh" | bash -s main
   # PATCH SUSFS for GKI 5.10
   if [ "$KVER" == "5.10" ]; then
     log "Applying SUSFS patches for GKI 5.10 (ReSukiSU Method)..."
