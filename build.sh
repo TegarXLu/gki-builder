152911f0fa3018b276491cbb5fb30c0ff0f9239a
TegarXLu <botdimas67@gmail.com>
2026-03-16 12:44:16 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -392,7 +392,7 @@ fi
 export KBUILD_BUILD_USER="$USER"
 export KBUILD_BUILD_HOST="$HOST"
 export KBUILD_BUILD_TIMESTAMP=$(date)
-export KCFLAGS="-O2 -mcpu=cortex-a76"
+export KCFLAGS="-w"
 
 # =============================================================================
 # MAKE ARGS - FIXED
